49c3f965333c592c5420696423f0e07caa03b295
Brian D. Weitzner <brian.weitzner@gmail.com>
2022-04-15 17:07:56 -0600

    Initial commit supporting local and git-based builds of rosetta_omp

--- docker_build.sh
+++ docker_build.sh
@@ -10,7 +10,7 @@ function usage() {
 }
 
 if [ -z "${3:-}" ]; then usage; fi
-if [[ "${1:-}" != "rosetta_omp" && "${1:-}" != "rifdock" ]]; then usage; fi
+if [[ "${1:-}" != "rosetta_omp" && "${1:-}" != "rosetta_local" && "${1:-}" != "rifdock" ]]; then usage; fi
 
 THISDIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
 THISDIR=$(realpath $THISDIR)
@@ -18,6 +18,23 @@ ROOT=$THISDIR/${1:-}
 ARTIFACTS=$(realpath ${2:-})
 GH_TOKEN=$(cat ${3:-})
 UPLOAD_PACKAGES=False
+
+# these two variables are used to control the rosetta build - they are not set when builing rifdock
+APPEND_YAML=""
+VOLUMES=""
+if [[ "${1:-}" == "rosetta_omp" ]]; then
+  # need to include an additional yaml file in the conda build command below
+  APPEND_YAML="--append-file /home/conda/root/meta.git.yaml"
+fi
+
+if [[ "${1:-}" == "rosetta_local" ]]; then
+  # adjust directory to point to the correct conda recipe
+  ROOT=$THISDIR/rosetta_omp
+  # need to include an additional yaml file in the conda build command below
+  APPEND_YAML="--append-file /home/conda/root/meta.local.yaml"
+  # include an additional volume to mount the local rosetta installation
+  VOLUMES="-v $THISDIR/../rosetta_src_2018.09.60072_bundle/main:/home/conda/root/rosetta"
+fi
 
 # Conda-build copies work tree into build root, so build root must be outside
 # tree to avoid recursive copy.
@@ -49,11 +66,12 @@ set -x
 docker run \
            -v "${ROOT}":/home/conda/root:rw,z \
            -v "${ARTIFACTS}":/home/conda/build:rw,z \
+           "${VOLUMES}" \
            -e BINSTAR_TOKEN \
            -e HOST_USER_ID=$HOST_USER_ID \
            -e UPLOAD_PACKAGES=$UPLOAD_PACKAGES \
            $DOCKER_IMAGE \
-           /bin/bash -c "git config --global url.\"https://${GH_TOKEN}:@github.com/\".insteadOf \"https://github.com/\" && conda build /home/conda/root --croot /home/conda/build"
+           /bin/bash -c "git config --global url.\"https://${GH_TOKEN}:@github.com/\".insteadOf \"https://github.com/\" && conda build ${APPEND_YAML} /home/conda/root --croot /home/conda/build"
 
 # if you're on a mac and the fs performance is terrible, maybe you want to set
 # some volumes on the vm? It's like 1000x faster.
